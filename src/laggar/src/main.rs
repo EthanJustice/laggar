6ac806ae174140bce25cead23204a130ae0849bb
EthanJustice <47954690+EthanJustice@users.noreply.github.com>
2020-07-01 17:48:39 -0400

    feat: cli parsing with clap

--- src/laggar/src/main.rs
+++ src/laggar/src/main.rs
@@ -6,5 +6,17 @@ use termimad::*;
 use clap::*;
 
 fn main() {
-    
+	let clap = clap::App::new("Laggar")
+		.version("0.1.0")
+		.author("Ethan Justice")
+		.about("site to markdown converter")
+		.arg(clap::Arg::with_name("download")
+			.short("d")
+			.long("download")
+			.help("Download and convert a website")
+			.takes_value(true)
+			.required(true)
+		).get_matches();
+
+	let url = clap.value_of("download").unwrap();
 }
