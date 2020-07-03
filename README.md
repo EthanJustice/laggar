72e43600e1da75bb565b8482ac455fc2887cb03d
EthanJustice <47954690+EthanJustice@users.noreply.github.com>
2020-07-02 22:13:09 -0400

    feat: docs

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # laggar
  site-to-markdown converter
+
+## Build
+
+To build, download the repository, then navigate to src/laggar and run `cargo build --release`.
+
+## Usage
+
+1. Navigate to the folder where you've installed the `laggar.exe` binary ([Build instructions](#build))
+2. Run `laggar {command}` (see [Commands](#commands))
+
+### Commands
+
+`-d, --download <URL>` - downloads the specified page
+
+```plaintext
+laggar --d https://example.com/
+```
+
+`-h, --help` - lists all commands
+
+```plaintext
+laggar -h
+```
+
+`-V` (note the capital) - lists the version
+
+```plaintext
+laggar -V
+```
--- src/laggar/src/main.rs
+++ src/laggar/src/main.rs
@@ -1,5 +1,6 @@
 #![allow(unused_must_use)]
 
+// std imports
 use std::fs;
 use std::path::Path;
 use std::io::{stdout, Write};
@@ -17,7 +18,7 @@ use reqwest::blocking;
 use clap::{App, Arg};
 
 fn main() {
-	set_status(style("Laggar:\n"));
+	set_status(style("Laggar:\n")); // Sets terminal window to "Laggar"
 	let clap = App::new("Laggar")
 		.version("0.1.0")
 		.author("Ethan Justice")
@@ -30,39 +31,40 @@ fn main() {
 			.required(true)
 		).get_matches();
 
-	let url = parse_url(String::from(clap.value_of("download").unwrap()));
+	let url = parse_url(String::from(clap.value_of("download").unwrap())); // Parsed url (inner is raw user input)
 	
 	set_status(style("Starting download...\n").with(Color::Cyan));
 	
-	let site = get_site(&url);
+	let site = get_site(&url); // HTML from url
 
 	set_status(style(&format!("Downloaded {}.\n", &url.as_str())[..]).with(Color::Yellow));
 
 	let md = match site {
-		Ok(data) => {
+		Ok(data) => { // site scraping goes smoothly
 			set_status(style("Parsing...\n").with(Color::Cyan));
-			parse_html(data.as_str())
+			parse_html(data.as_str()) // converts html to markdown
 		},
-		Err(error) => {
+		Err(error) => { // site scraping fails
 			set_status(style(&format!("\n\nFailed to download site.\nError: {}\n", error)[..]).with(Color::Red));
-			exit(1)
+			exit(1) // exits process
 		}
 	};
 
 	set_status(style("Creating file...\n").with(Color::Cyan));
 
 	match create_file(md, &url) {
+		// generating file/directory goes smoothly
 		Ok(path) => set_status(style(&format!("Created file at {}.\n", Path::new(&path).display())[..]).with(Color::Yellow)),
-		Err(error) => {
+		Err(error) => { // generating file/directory fails
 			set_status(style(&format!("\n\nFailed to generate markdown.\nError: {}\n", error)[..]).with(Color::Red));
-			exit(1)
+			exit(1) // exits process
 		}
 	};
 
 	set_status(style("\nFinished successfully.\n").with(Color::Green).attribute(Attribute::Underlined));
 }
 
-fn parse_url(mut url: String) -> String {
+fn parse_url(mut url: String) -> String { // Parses user-provided input
 	if url.starts_with("http") == false {
 		url.insert_str(0, "http://");
 	}
@@ -70,14 +72,14 @@ fn parse_url(mut url: String) -> String {
 	url
 }
 
-fn get_site(url: &String) -> std::result::Result<String, std::boxed::Box<dyn std::error::Error>> {
+fn get_site(url: &String) -> std::result::Result<String, std::boxed::Box<dyn std::error::Error>> { // Scrapes HTML from specified url
 	let site = blocking::get(url)?
 		.text()?;
 	
 	Ok(site)
 }
 
-fn create_file(markdown: String, url: &str) -> Result<String> {
+fn create_file(markdown: String, url: &str) -> Result<String> { // Generates file (and "content" directory if necessary) and fills it with markdown from html
 	if Path::new("./content/").is_dir() == false { create_directory() }
 
 	let new_url = url.replace("https://", "").replace("http://", "").replace("/", ".");
@@ -90,11 +92,11 @@ fn create_file(markdown: String, url: &str) -> Result<String> {
 	Ok(path)
 }
 
-fn create_directory() {
+fn create_directory() { // Creates directory if needed
 	fs::create_dir("./content").expect("Failed to create directory.");
 }
 
-fn set_status(status: StyledContent<&str>) -> Result<()> {
+fn set_status(status: StyledContent<&str>) -> Result<()> { // Prints status to terminal; sets terminal window title
 	execute!(
 		stdout(),
 
