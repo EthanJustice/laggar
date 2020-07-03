b0af9f1ac43e4b139a3181391e7ed2c0a3962c49
EthanJustice <47954690+EthanJustice@users.noreply.github.com>
2020-07-02 20:22:16 -0400

    feat: coloured terminal output

--- src/laggar/src/main.rs
+++ src/laggar/src/main.rs
@@ -1,13 +1,23 @@
+#![allow(unused_must_use)]
+
 use std::fs;
 use std::path::Path;
+use std::io::{stdout, Write};
+use std::process::exit;
 
 // External crates
 use html2md::parse_html;
-use crossterm::*;
+use crossterm::{
+	terminal::SetTitle,
+	style::{ Print, style, Color, StyledContent, Attribute },
+	execute,
+	Result
+};
 use reqwest::blocking;
 use clap::{App, Arg};
 
 fn main() {
+	set_status(style("Laggar:\n"));
 	let clap = App::new("Laggar")
 		.version("0.1.0")
 		.author("Ethan Justice")
@@ -21,15 +31,35 @@ fn main() {
 		).get_matches();
 
 	let url = parse_url(String::from(clap.value_of("download").unwrap()));
-
-	let site = get_site(&url);
 	
+	set_status(style("Starting download...\n").with(Color::Cyan));
+	
+	let site = get_site(&url);
+
+	set_status(style(&format!("Downloaded {}.\n", &url.as_str())[..]).with(Color::Yellow));
+
 	let md = match site {
-		Ok(data) => parse_html(data.as_str()),
-		Err(error) => panic!("Failed to download site: {}", error)
+		Ok(data) => {
+			set_status(style("Parsing...\n").with(Color::Cyan));
+			parse_html(data.as_str())
+		},
+		Err(error) => {
+			set_status(style(&format!("\n\nFailed to download site.\nError: {}\n", error)[..]).with(Color::Red));
+			exit(1)
+		}
 	};
 
-	create_file(md, &url);
+	set_status(style("Creating file...\n").with(Color::Cyan));
+
+	match create_file(md, &url) {
+		Ok(path) => set_status(style(&format!("Created file at {}.\n", Path::new(&path).display())[..]).with(Color::Yellow)),
+		Err(error) => {
+			set_status(style(&format!("\n\nFailed to generate markdown.\nError: {}\n", error)[..]).with(Color::Red));
+			exit(1)
+		}
+	};
+
+	set_status(style("\nFinished successfully.\n").with(Color::Green).attribute(Attribute::Underlined));
 }
 
 fn parse_url(mut url: String) -> String {
@@ -47,7 +77,7 @@ fn get_site(url: &String) -> std::result::Result<String, std::boxed::Box<dyn std
 	Ok(site)
 }
 
-fn create_file(markdown: String, url: &str) -> std::io::Result<()> {
+fn create_file(markdown: String, url: &str) -> Result<String> {
 	if Path::new("./content/").is_dir() == false { create_directory() }
 
 	let new_url = url.replace("https://", "").replace("http://", "").replace("/", ".");
@@ -55,11 +85,23 @@ fn create_file(markdown: String, url: &str) -> std::io::Result<()> {
 	let mut path = String::from("content/") + &new_url + ".md";
 	path = path.replace("..", ".");
 
-	fs::write(path, markdown.as_bytes())?;
+	fs::write(&path, markdown.as_bytes())?;
 
-	Ok(())
+	Ok(path)
 }
 
 fn create_directory() {
 	fs::create_dir("./content").expect("Failed to create directory.");
+}
+
+fn set_status(status: StyledContent<&str>) -> Result<()> {
+	execute!(
+		stdout(),
+
+		SetTitle(status.content()),
+
+		Print(status)
+	);
+
+	Ok(())
 }
